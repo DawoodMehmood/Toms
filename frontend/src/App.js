fab14c3128fdf7337528a5e996e82a6c5383bae3
Muhammad Abdullah Khan Abbasi <rykabdullah7@gmail.com>
2024-02-29 23:37:08 +0500

    route setup

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,9 +1,15 @@
 import TermsAndConditions from "./pages/terms";
+import { Routes, Route } from "react-router-dom";
 
 function App() {
   return (
     <div className="App">
-      <TermsAndConditions />
+      <Routes>
+        <Route
+          path="/terms-and-conditions"
+          element={<TermsAndConditions />}
+        ></Route>
+      </Routes>
     </div>
   );
 }
--- frontend/src/index.js
+++ frontend/src/index.js
@@ -3,13 +3,14 @@ import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "./App";
 import { IconContext } from "react-icons";
-
+import { BrowserRouter } from "react-router-dom";
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <IconContext.Provider value={{ className: "small-size" }}>
-      <App />
-    </IconContext.Provider>
-    ;
+    <BrowserRouter>
+      <IconContext.Provider value={{ className: "small-size" }}>
+        <App />
+      </IconContext.Provider>
+    </BrowserRouter>
   </React.StrictMode>
 );
