8bf81ef7ab77cc4287ae1a2fa9ceadfd3759c716
Dawood Mehmood <dawoodmehmood52222@gmail.com>
2024-02-29 21:37:01 +0500

    terms page frontend

---
+++ frontend/src/pages/terms.jsx
@@ -0,0 +1,289 @@
+import React from "react";
+import HeadingWithPara from "../components/headingWithPara";
+
+const TermsAndConditions = () => {
+  return (
+    <>
+      <div className="mx-20">
+        <h1 className="flex items-center justify-center">Terms of Service</h1>
+        <HeadingWithPara
+          heading={<p>Promotions & Discounts</p>}
+          content={
+            <p>
+              All promotions and discounts are only valid during the period of
+              the applicable promotion. Once the promotional period has ended,
+              the discount can no longer be applied to orders.
+            </p>
+          }
+        />
+        <HeadingWithPara
+          heading={<p>ONGOING PROMOTIONS</p>}
+          content={
+            <p>
+              All ongoing promotions applied to incentivised actions such as
+              leaving a review, <strong>exclude final sale items.</strong>
+            </p>
+          }
+        />
+        <HeadingWithPara
+          heading={<p>INSTAGRAM GIVEAWAY</p>}
+          content={
+            <p>
+              Competition begins Wednesday 17th January 6:00pm AEST and ends
+              Saturday 24th February 9am AEST. Entrants must like the giveaway
+              post, ensure they're following @vrg.grl on Instagram and leave a
+              comment for the chance to win. (3) winners will be drawn at random
+              at the close of the competition and will be contacted via
+              Instagram direct message. Competition is open worldwide. The
+              winner will also be announced via Instagram stories.<br></br>
+              ________________________________
+              <br></br>
+              <br></br>Your access to our website is conditional upon your
+              acceptance and compliance with the terms, conditions, notices and
+              disclaimers contained in this document (collectively known as
+              Terms of Use). Your use of, and/or access to, our website
+              constitutes your agreement to the Terms of Use. We reserve the
+              right to amend the Terms of Use at any time. Although we may
+              notify you of any amendment, revision or modification, it is your
+              responsibility to periodically review the Terms of Use. You agree
+              to be bound by such changes, modifications or revisions as made by
+              us from time to time.
+            </p>
+          }
+        />
+        <HeadingWithPara
+          heading={<p>Our services:</p>}
+          content={
+            <p>
+              We cannot be responsible for any delays or interruptions to the
+              Website. We will use commercially reasonable efforts to minimise
+              delays and interruptions. However, we cannot warrant that the
+              Website will be available at all times or at any given time.
+            </p>
+          }
+        />
+        <HeadingWithPara
+          heading={<p>Prohibited conduct:</p>}
+          content={
+            <p>
+              In relation to our website, you must not:<br></br>
+              use our website for any activities or post or transmit via our
+              website, any information or materials which breach any laws or
+              regulations, infringe a third party’s rights, or are contrary to
+              any relevant standard or codes; use our website to post or
+              transmit any material which interferes with other users or
+              defames, harasses, threatens, menaces, offends or restricts any
+              person or which inhibits any user from using our website or the
+              Internet; use our website to send unsolicited email messages; in
+              any way tamper with, hinder or modify our website; knowingly
+              transmit any viruses or other disabling features to our website or
+              via our website; or attempt any of the above acts or facilitate or
+              assist another person to do any of the above acts.
+            </p>
+          }
+        />
+        <HeadingWithPara
+          heading={<p>Intellectual property:</p>}
+          content={
+            <p>
+              The material on our website, including the software, design, text,
+              images and graphics comprised in our website and the selection and
+              layout of our website are owned or under license by Verge Girl and
+              protected by Australian and international laws. Additional
+              graphics may be sourced from our blog, from which we respect the
+              intellectual property rights of others and claim no commercial
+              benefit from. Your use of our website does not grant you a licence
+              or act as a right of use of any of the trade marks or logos,
+              whether registered or unregistered, that are displayed on our
+              website without the express written permission of the trade mark
+              owner. We own the copyright, which subsists in all creative and
+              literary works that are displayed on our website. You may view our
+              website and its content using your web browser. In visiting our
+              website, you may make a temporary copy of our website by means of
+              the usual operation of your web browser only.
+            </p>
+          }
+        />
+        <HeadingWithPara
+          heading={<p>You must not:</p>}
+          content={
+            <p>
+              Reproduce or use any of the material on the Website for commercial
+              purposes, including sale; in any way modify the material on the
+              Website; or cause any of the material on the Website to be framed
+              or embedded in another website. In summary, you must not modify,
+              copy, reproduce, republish, frame, upload to a third party, post,
+              transmit or distribute the content of our website in any way
+              except as expressly provided for by us or expressly authorised in
+              writing by us. In the event that you do any of the above acts, we
+              will not hesitate to enforce our intellectual property rights
+              against you.
+            </p>
+          }
+        />
+        <HeadingWithPara
+          heading={<p>Third party links:</p>}
+          content={
+            <p>
+              The website may contain hyperlinks and other pointers to websites
+              operated by third parties (“Linked Websites”). We do not control
+              linked websites and are therefore not responsible for the content
+              of any linked website or any hyperlink contained in a linked
+              website. We provide the hyperlinks for your convenience only and
+              do not indicate, expressly or implicitly, any endorsement,
+              sponsorship or approval by us of a linked website or the products
+              or services offered at linked websites. You visit linked websites
+              entirely at your own risk. We do not provide any warranty or take
+              any responsibility for any aspect of linked websites or their
+              content. You should make your own investigations with respect to
+              the suitability of goods and/or services offered to you via a
+              linked website.
+            </p>
+          }
+        />
+        <HeadingWithPara
+          heading={<p>Indemnity:</p>}
+          content={
+            <p>
+              By using our website, you agree to indemnify us from and against
+              all actions, claims, suits, demands, damages, liabilities, costs
+              or expenses (whether in tort or in contract including and without
+              limitation, negligence) arising out of or in any way connected to
+              the use of our website by you.
+            </p>
+          }
+        />
+        <HeadingWithPara
+          heading={<p>Disclaimer:</p>}
+          content={
+            <p>
+              Some legislation such as the Australian Consumer and Competition
+              Act 2010 and other similar consumer protection laws and
+              regulations in other countries may confer you with rights and
+              remedies relating to the provision of goods or services to you by
+              us via our website which cannot be excluded, restricted or
+              modified (your “Statutory Rights”). We exclude all conditions and
+              warranties implied by custom, law or statute except for your
+              Statutory Rights. Except for your Statutory Rights and with
+              respect to the Website: all material on our website is provided to
+              you without warranties of any kind, either express or implied; we
+              expressly disclaim all warranties of any kind including but not
+              limited to warranties of acceptable quality and fitness for a
+              particular purpose; we do not warrant that the functions contained
+              in any material on our website or your access to our website will
+              be uninterrupted or error free, that any defects will be corrected
+              or that our website or the server which stores and transmits
+              material to you are free of viruses or any other harmful
+              components; and we do not warrant or make any representation
+              regarding your access to, or the results of your access to, our
+              website including its correctness, accuracy, timeliness,
+              completeness, reliability or otherwise. To the extent permitted by
+              law, including but not limited to any act or omission on your
+              part, we will not be liable for any loss, damage, costs or expense
+              whether direct, indirect, incidental, special and/or
+              consequential, including loss of profits, suffered by you or
+              claims made against you which result from any use or access of, or
+              any inability to use or access, our website. You expressly
+              acknowledge that we do not exert control over users of the
+              Internet and we are not liable for damage suffered by you, either
+              directly or indirectly, as a result of your access to our website.
+            </p>
+          }
+        />
+
+        <HeadingWithPara
+          heading={<p>Limitation of liability:</p>}
+          content={
+            <p>
+              To the extent permitted by law, our liability for breach of any
+              implied warranty or condition, which cannot be excluded by the
+              terms of use, is limited, at our option, to one or more of the
+              following: In the case of goods supplied or offered by us: the
+              replacement of the goods or the supply of equivalent goods; the
+              repair of such goods; the payment of the costs of replacing the
+              goods or acquiring equivalent goods; or the payment of the costs
+              of having the goods repaired.
+            </p>
+          }
+        />
+        <HeadingWithPara
+          heading={<p>Termination:</p>}
+          content={
+            <p>
+              The terms of use are effective until terminated by us, which we
+              may do at any time and without notice to you. In the event of
+              termination, all restrictions imposed on you by the terms of use
+              and limitations of liability set out in the terms of use will
+              survive.
+            </p>
+          }
+        />
+        <HeadingWithPara
+          heading={<p>Miscellaneous:</p>}
+          content={
+            <p>
+              We rely upon your continued observance of the terms of use. If we
+              suffer loss or damage or incur any costs associated with any
+              breach by you of the terms of use or any associated legal
+              obligation, you agree to indemnify us for those losses, damages
+              and costs. We do not make any claims that the information is
+              appropriate or may be downloaded in all areas, countries or
+              jurisdictions. Access to the information contained in our website
+              may not be legal by certain persons or in certain countries. If
+              you access the Website, you do so at your own risk and you are
+              responsible for compliance with the laws of your jurisdiction. If
+              any provision of the terms of use is found to be invalid or
+              unenforceable by a Court of Law, such invalidity or
+              unenforceability will not affect the remainder of the document,
+              which will continue in full force and effect. All rights not
+              expressly granted in the terms of use are reserved. If we do not
+              act in relation to a breach of the terms of use by you, this does
+              not waive our rights to act with respect to subsequent or similar
+              breaches of the terms of use by you.
+            </p>
+          }
+        />
+        <HeadingWithPara
+          heading={<p>Applicable law:</p>}
+          content={
+            <p>
+              The terms of use are governed by and construed in accordance with
+              the laws of the State of Queensland, Australia. You irrevocably
+              and unconditionally submit to the non-exclusive jurisdiction of
+              the Courts of the Queensland and Courts of Appeal from them for
+              determining any dispute concerning the terms of use.
+            </p>
+          }
+        />
+        <HeadingWithPara
+          heading={<p>Text Marketing and notifications:</p>}
+          content={
+            <p>
+              By entering your phone number in the checkout and initializing a
+              purchase, subscribing via our subscription form or a keyword, you
+              agree that we may send you text notifications (for your order,
+              including abandoned cart reminders) and text marketing offers.
+              Text marketing messages will not exceed 10 a month. You
+              acknowledge that consent is not a condition for any purchase. If
+              you wish to unsubscribe from receiving text marketing messages and
+              notifications reply with STOP to any mobile message sent from us
+              or use the unsubscribe link we provided you within any of our
+              messages. You understand and agree that alternative methods of
+              opting out, such as using alternative words or requests will not
+              be accounted as a reasonable means of opting out. Message and data
+              rates may apply. For any questions please text HELP to the number
+              you received the messages from. You can also contact us for more
+              information. If you wish to opt out please follow the procedures
+              above.
+            </p>
+          }
+        />
+        <div className="my-6">
+          <strong>Last updated 6 NOV 2023</strong>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default TermsAndConditions;
