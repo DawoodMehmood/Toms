1e6661165d04977197914d97d49c866b2f702292
Dawood Mehmood <dawoodmehmood52222@gmail.com>
2024-02-29 21:38:51 +0500

    Add css for h1, links of faqs and size of icon

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -2,10 +2,14 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "./App";
+import { IconContext } from "react-icons";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <App />
+    <IconContext.Provider value={{ className: "small-size" }}>
+      <App />
+    </IconContext.Provider>
+    ;
   </React.StrictMode>
 );
