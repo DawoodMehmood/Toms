1ca59f93061b76861f621e1077e341ad5eff7159
Dawood Mehmood <dawoodmehmood52222@gmail.com>
2024-03-04 03:33:42 +0500

    Made canvas image with text component

---
+++ frontend/src/components/canvasImageWithText.jsx
@@ -0,0 +1,22 @@
+import React from "react";
+
+const CanvasImage = ({ imageUrl, path, title, description }) => {
+  return (
+    <section>
+      <div className="w-full my-10 relative">
+        <img src={imageUrl} alt="canvas" />
+        <div className="absolute top-0 left-0 right-0 bottom-0 flex flex-col justify-center items-center gap-5 text-white  drop-shadow-lg">
+          <p className="text-5xl font-semibold tracking-4">{title}</p>
+          <p className="font-thin text-2xl text-white">{description}</p>
+          <a href={path} className="decoration-none">
+            <button className="tracking-2 border px-4 py-2 hover:bg-orangeColor text-xl">
+              SHOP NOW
+            </button>
+          </a>
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default CanvasImage;
