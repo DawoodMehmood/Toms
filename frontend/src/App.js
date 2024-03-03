45e127c8e40d6edf6d212e8f20d1ddc1a316b433
Dawood Mehmood <dawoodmehmood52222@gmail.com>
2024-03-03 19:18:56 +0500

    Add new components to Appjs

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,13 +1,14 @@
 import TermsAndConditions from "./pages/terms";
 import { Routes, Route } from "react-router-dom";
 import FAQS from "./pages/faqs";
+import Home from "./pages/home";
 import Careers from "./pages/careers";
-import "./components/App.css";
+import "./components/components.css";
 import ProductDetails from "./pages/productDetails";
 
 function App() {
   return (
-    <div className="App">
+    <div className="App max-w-[1440px]">
       <Routes>
         <Route
           path="/terms-and-conditions"
@@ -17,6 +18,7 @@ function App() {
         <Route path="/faqs" element={<FAQS />}></Route>
         <Route path="/careers" element={<Careers />}></Route>
         <Route path="/product-details" element={<ProductDetails />}></Route>
+        <Route path="/home" element={<Home />}></Route>
       </Routes>
     </div>
   );
