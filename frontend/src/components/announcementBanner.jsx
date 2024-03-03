6df8281dd9f692363018fb767b5609ad10ae7f9b
Dawood Mehmood <dawoodmehmood52222@gmail.com>
2024-03-03 19:20:06 +0500

    Made announcement banner component

---
+++ frontend/src/components/announcementBanner.jsx
@@ -0,0 +1,38 @@
+import React, { useState, useEffect } from "react";
+
+const Banner = () => {
+  const messages = [
+    { text: "FAST SHIPPING • INSTANT REFUNDS", link: "/pages/returns" },
+    { text: "NEW ARRIVALS OUT NOW", link: "/collection/new" },
+  ];
+  const [currentMessageIndex, setCurrentMessageIndex] = useState(0);
+  const [opacity, setOpacity] = useState("opacity-100");
+
+  useEffect(() => {
+    const changeMessage = () => {
+      setOpacity("opacity-0");
+      setTimeout(() => {
+        setCurrentMessageIndex((prevIndex) => (prevIndex === 0 ? 1 : 0));
+        setOpacity("opacity-100");
+      }, 1000);
+    };
+
+    const intervalId = setInterval(changeMessage, 4000);
+
+    return () => clearInterval(intervalId);
+  }, []);
+
+  return (
+    <section className="overflow-hidden bg-banner">
+      <div
+        className={`h-[40px] flex items-center justify-center transition-opacity duration-1000 ${opacity} text-sm tracking-widest unselectable`}
+      >
+        <a href={messages[currentMessageIndex].link} className="">
+          {messages[currentMessageIndex].text}
+        </a>
+      </div>
+    </section>
+  );
+};
+
+export default Banner;
