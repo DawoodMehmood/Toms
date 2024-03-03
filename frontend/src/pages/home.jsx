d2b09746c627602f1dbc5e5c72bcfc2bc9425c5e
Dawood Mehmood <dawoodmehmood52222@gmail.com>
2024-03-04 03:35:17 +0500

    Updated home page with new components

--- frontend/src/pages/home.jsx
+++ frontend/src/pages/home.jsx
@@ -1,14 +1,16 @@
 import React from "react";
 import Video from "../components/videoJumbotron";
-import Banner from "../components/announcementBanner";
 import CanvasImage from "../components/canvasImage";
+import CanvasImageWithText from "../components/canvasImageWithText";
 import canvasImage1 from "../assets/img/canvasImage.jpg";
+import canvasImage2 from "../assets/img/canvasImage2.jpg";
 import Gallery from "../components/gallery";
 import image1 from "./../assets/img/pro-1.jpg";
 import image2 from "./../assets/img/pro-2.jpg";
 import image3 from "./../assets/img/pro-3.jpg";
 import image4 from "./../assets/img/pro-4.jpg";
 import Slider from "../components/imageSlider";
+import ShapeCarousel from "../components/shapeCarousel";
 
 const Home = () => {
   const items = [
@@ -39,18 +41,23 @@ const Home = () => {
   ];
   return (
     <div>
-      <Banner />
       <Video />
       <Slider title={"NEW IN"} />
-      <CanvasImage imageUrl={canvasImage1} path={"/mez"} />
+      <CanvasImageWithText
+        imageUrl={canvasImage1}
+        path={"/mez"}
+        title={"JANUARY 24 STOCKS"}
+        description={"CULT FAVOURITES ARE BACK"}
+      />
       <Slider title={"MEZ"} />
       <section className="my-5">
         <div className="bg-banner flex items-center justify-center h-[60px] tracking-wider font-bold">
           <p>INSPIRING CREATIVITY & CONFIDENCE THROUGH STYLE</p>
         </div>
       </section>
-      <CanvasImage imageUrl={canvasImage1} path={"/shop"} />
+      <CanvasImage imageUrl={canvasImage2} path={"/shop"} />
       <Gallery items={items} />
+      <ShapeCarousel />
     </div>
   );
 };
