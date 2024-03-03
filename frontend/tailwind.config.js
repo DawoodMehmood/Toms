ea4aa43e01b1cd352172f591cf041fc1afb051df
Dawood Mehmood <dawoodmehmood52222@gmail.com>
2024-03-03 19:18:14 +0500

    Add custom colors

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -2,7 +2,12 @@
 module.exports = {
   content: ["./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        banner: "#faf5ef",
+        orangeColor: "#f69420",
+      },
+    },
   },
   plugins: [],
 };
