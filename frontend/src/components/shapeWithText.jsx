59e8c1ce510b0ec377f5b7bfa472b04c00821308
Dawood Mehmood <dawoodmehmood52222@gmail.com>
2024-03-04 03:35:44 +0500

    Made shape with text component

---
+++ frontend/src/components/shapeWithText.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+const ShapeWithText = ({ path, data }) => {
+  return (
+    <div className="flex flex-col items-center justify-center">
+      <img src={path} alt="shape" className="w-[126px] h-[126px]" />
+      <p>{data}</p>
+    </div>
+  );
+};
+
+export default ShapeWithText;
