94e77394726acabc0e8d4e6b4546af52761d2985
Dawood Mehmood <dawoodmehmood52222@gmail.com>
2024-03-07 22:08:19 +0500

    Admin home component

---
+++ frontend/src/admin/pages/home.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import ProductForm from "../components/productForm";
+
+const AdminHome = () => {
+  return (
+    <div className="flex items-center justify-center my-10">
+      <ProductForm />
+    </div>
+  );
+};
+
+export default AdminHome;
