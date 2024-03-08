585d26d64aa391a087640cbb93a1318eb61eb9c1
Dawood Mehmood <dawoodmehmood52222@gmail.com>
2024-03-08 20:56:48 +0500

    Add new routes to handle fetching of products by subcategoryid

--- backend/src/routes/subcategoryRoutes.js
+++ backend/src/routes/subcategoryRoutes.js
@@ -7,5 +7,10 @@ router.get("/", subcategoryController.getAllSubcategories);
 router.get("/:id", subcategoryController.getSubcategoryById);
 router.put("/:id", subcategoryController.updateSubcategory);
 router.delete("/:id", subcategoryController.deleteSubcategory);
+// Get products of a subcategory
+router.get(
+  "/products/:subcategoryId",
+  subcategoryController.getProductsBySubcategory
+);
 
 module.exports = router;
