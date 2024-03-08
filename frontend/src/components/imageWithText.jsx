9354af1053dc693ccaad570f4e4397d6c74477c3
Dawood Mehmood <dawoodmehmood52222@gmail.com>
2024-03-08 21:04:29 +0500

    Made image with text component dynamic data

--- frontend/src/components/imageWithText.jsx
+++ frontend/src/components/imageWithText.jsx
@@ -1,16 +1,29 @@
 import React from "react";
+import { useNavigate } from "react-router-dom";
+import image1 from "./../assets/img/pro-1.jpg";
 
-const ImageWithText = ({ imageUrl, text, path }) => {
+const ImageWithText = ({ category }) => {
+  const navigate = useNavigate();
+  const formattedName = category.category_name.replace(/ /g, "-").toLowerCase();
+
+  const handleCardClick = () => {
+    navigate(`/category/${formattedName}`, {
+      state: { name: category.category_name, categoryId: category.category_id },
+    });
+  };
   return (
     <div className="relative group">
-      <a href={path} className="block hover:underline underline-offset-4">
-        <img src={imageUrl} alt="" />
+      <div
+        onClick={handleCardClick}
+        className="cursor-pointer block hover:underline underline-offset-4"
+      >
+        <img src={image1} alt="" />
         <div className="absolute top-0 left-0 right-0 bottom-0 flex justify-center items-center">
           <p className="font-bold text-xl text-white group-hover:underline decoration-4 transition-all duration-300 ease-in-out">
-            {text}
+            {category.category_name.toUpperCase()}
           </p>
         </div>
-      </a>
+      </div>
     </div>
   );
 };
