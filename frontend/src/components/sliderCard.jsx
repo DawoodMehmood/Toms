69cc336c8edbcb49ca77047845fce83dcc5164bc
Dawood Mehmood <dawoodmehmood52222@gmail.com>
2024-03-08 21:05:13 +0500

    Made sliderCard data dynamic

--- frontend/src/components/sliderCard.jsx
+++ frontend/src/components/sliderCard.jsx
@@ -1,18 +1,29 @@
 import React, { useState } from "react";
 import { IoMdHeartEmpty, IoMdHeart } from "react-icons/io";
+import { useNavigate } from "react-router-dom";
 
-const SliderCard = ({ imageUrl, hoverImageUrl, title, price, isSoldOut }) => {
+const SliderCard = ({ product }) => {
   const [isFavorited, setIsFavorited] = useState(false);
   const [isHovering, setIsHovering] = useState(false);
+  const navigate = useNavigate();
+  const imageUrl = product.image_urls[0];
+  const hoverImageUrl = product.image_urls[1];
+  const formattedName = product.product_name.replace(/ /g, "-").toLowerCase();
 
   const toggleFavorite = (event) => {
     event.preventDefault();
     setIsFavorited(!isFavorited);
   };
 
+  const handleCardClick = () => {
+    navigate(`/product-details/${formattedName}`, {
+      state: { productData: product },
+    });
+  };
+
   return (
     <div>
-      <a href={`/product-details`}>
+      <div onClick={handleCardClick} className="cursor-pointer">
         <div
           className="max-w-sm bg-white relative"
           onMouseEnter={() => setIsHovering(true)}
@@ -22,14 +33,18 @@ const SliderCard = ({ imageUrl, hoverImageUrl, title, price, isSoldOut }) => {
             <img
               className="w-full"
               src={isHovering ? hoverImageUrl || imageUrl : imageUrl}
-              alt={title}
+              alt={product.product_name}
             />
             <div className="mt-2 flex items-start justify-between">
-              <div className="flex flex-col items-start justify-center">
-                <div className="text-base">{title}</div>
-                <div className="text-gray-700 text-sm">{price}</div>
+              <div className="flex flex-col items-start justify-start">
+                <div className="text-base text-left whitespace-normal">
+                  {product.product_name.toUpperCase()}
+                </div>
+                <div className="text-gray-700 text-sm text-left">
+                  {product.price} AED
+                </div>
               </div>
-              <button className="" onClick={toggleFavorite}>
+              <button onClick={toggleFavorite}>
                 {isFavorited ? (
                   <IoMdHeart size={25} />
                 ) : (
@@ -38,13 +53,13 @@ const SliderCard = ({ imageUrl, hoverImageUrl, title, price, isSoldOut }) => {
               </button>
             </div>
           </div>
-          {isSoldOut && (
+          {/* {isSoldOut && (
             <div className="absolute right-2 top-2">
               <div className="p-2 bg-black text-white text-sm">SOLD OUT</div>
             </div>
-          )}
+          )} */}
         </div>
-      </a>
+      </div>
     </div>
   );
 };
