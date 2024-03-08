ac84fafa8b72258e7a2b254289926f4ce79eeae0
Dawood Mehmood <dawoodmehmood52222@gmail.com>
2024-03-08 20:57:53 +0500

    Segregated admin and public routes and add new public routes

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -5,30 +5,42 @@ import Home from "./pages/home";
 import Careers from "./pages/careers";
 import "./components/components.css";
 import ProductDetails from "./pages/productDetails";
-import Navbar from "./components/navbar";
 import ScrollToTopButton from "./components/scrollToTopButton";
-import Footer from "./components/footer";
 import AdminHome from "./admin/pages/home";
+import UserLayout from "./layouts/userLayout";
+import AdminLayout from "./layouts/adminLayout";
+import Category from "./pages/category";
 
 function App() {
   return (
     <div className="App max-w-[1440px] mx-auto">
-      <Navbar />
       <Routes>
-        <Route
-          path="/terms-and-conditions"
-          element={<TermsAndConditions />}
-        ></Route>
+        <Route element={<UserLayout />}>
+          <Route
+            path="/product-details/:id"
+            element={<ProductDetails />}
+          ></Route>
+          <Route
+            path="/pages/terms-and-conditions"
+            element={<TermsAndConditions />}
+          ></Route>
 
-        <Route path="/faqs" element={<FAQS />}></Route>
-        <Route path="/careers" element={<Careers />}></Route>
-        <Route path="/product-details" element={<ProductDetails />}></Route>
-        <Route path="/home" element={<Home />}></Route>
-        <Route path="/admin" element={<AdminHome />}></Route>
-        <Route path="/" element={<Navigate to="/home" />}></Route>
+          <Route path="/pages/faqs" element={<FAQS />}></Route>
+          <Route path="/category/:text" element={<Category />}></Route>
+          <Route path="/pages/careers" element={<Careers />}></Route>
+          <Route
+            path="/product-details/:id"
+            element={<ProductDetails />}
+          ></Route>
+          <Route path="/home" element={<Home />}></Route>
+          <Route path="/" element={<Navigate to="/home" />}></Route>
+        </Route>
+
+        <Route element={<AdminLayout />}>
+          <Route path="/admin" element={<AdminHome />}></Route>
+        </Route>
       </Routes>
       <ScrollToTopButton />
-      <Footer />
     </div>
   );
 }
