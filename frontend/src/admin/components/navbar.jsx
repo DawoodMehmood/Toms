46df185f11663785e85ed1a2ce05e842e3268637
Dawood Mehmood <dawoodmehmood52222@gmail.com>
2024-03-08 21:02:50 +0500

    Add admin navbar

---
+++ frontend/src/admin/components/navbar.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+const AdminNavbar = () => {
+  return (
+    <div>
+      <div className="flex items-center justify-center py-5 bg-banner tracking-widest font-extrabold">
+        ADMIN DASHBOARD
+      </div>
+    </div>
+  );
+};
+
+export default AdminNavbar;
