586a1d1d8ae6360f7885c90e0d4cb4b929080d74
Dawood Mehmood <dawoodmehmood52222@gmail.com>
2024-03-08 21:04:56 +0500

    Changed anchor to Link and added cartWithBadge component

--- frontend/src/components/navbar.jsx
+++ frontend/src/components/navbar.jsx
@@ -2,11 +2,12 @@ import React, { useState, useEffect } from "react";
 import { GoSearch } from "react-icons/go";
 import { RxHamburgerMenu } from "react-icons/rx";
 import { CiUser } from "react-icons/ci";
-import { HiOutlineShoppingBag } from "react-icons/hi2";
 import { RxCross1 } from "react-icons/rx";
 import logo from "./../assets/img/logo.png";
 import Banner from "./announcementBanner";
 import HeartWithBadge from "./heartWithBadge";
+import CartWithBadge from "./cartWithBadge";
+import { Link } from "react-router-dom";
 
 const Navbar = () => {
   const [isMenuOpen, setIsMenuOpen] = useState(false);
@@ -17,7 +18,7 @@ const Navbar = () => {
         <div className="bg-white shadow-md">
           <div className="grid grid-cols-3 p-7">
             <div
-              className=" cursor-pointer"
+              className=" cursor-pointer lg:opacity-0 lg:pointer-events-none"
               onClick={() => setIsMenuOpen(!isMenuOpen)}
             >
               {isMenuOpen ? (
@@ -27,18 +28,20 @@ const Navbar = () => {
               )}
             </div>
             <div className="text-center">
-              <img
-                src={logo}
-                alt="logo"
-                className="w-[120px] h-auto inline-block"
-              />
+              <Link to={"/"}>
+                <img
+                  src={logo}
+                  alt="logo"
+                  className="w-[120px] h-auto inline-block"
+                />
+              </Link>
             </div>
             <div className="text-gray-300 flex items-center justify-end gap-2">
               <GoSearch size={20} />
               <span className="text-sm">SEARCH</span>
-              <HeartWithBadge count={5} />
+              <HeartWithBadge count={15} />
               <CiUser size={26} />
-              <HiOutlineShoppingBag size={25} />
+              <CartWithBadge count={1} />
             </div>
           </div>
           <div className="py-5 px-7 flex items-end justify-between text-sm text-gray-500">
