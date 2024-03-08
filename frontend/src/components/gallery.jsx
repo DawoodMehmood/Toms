9c48890dabaaa485ff7a6f935dc1c2ce358298c2
Dawood Mehmood <dawoodmehmood52222@gmail.com>
2024-03-08 21:03:35 +0500

    Made gallery dynamic

--- frontend/src/components/gallery.jsx
+++ frontend/src/components/gallery.jsx
@@ -6,12 +6,7 @@ const Gallery = ({ items }) => {
     <section>
       <div className="m-10 grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-5">
         {items.map((item) => (
-          <ImageWithText
-            key={item.id}
-            imageUrl={item.imageUrl}
-            text={item.text}
-            path={item.path}
-          />
+          <ImageWithText key={item.category_id} category={item} />
         ))}
       </div>
     </section>
