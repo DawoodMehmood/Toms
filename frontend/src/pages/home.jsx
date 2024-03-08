c1862025ec0582e1bb1ef432ad520dea0d0cb6e4
Dawood Mehmood <dawoodmehmood52222@gmail.com>
2024-03-08 21:06:30 +0500

    Made data on public home dynamic

--- frontend/src/pages/home.jsx
+++ frontend/src/pages/home.jsx
@@ -1,62 +1,63 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import Video from "../components/videoJumbotron";
 import CanvasImage from "../components/canvasImage";
 import CanvasImageWithText from "../components/canvasImageWithText";
 import canvasImage1 from "../assets/img/canvasImage.jpg";
 import canvasImage2 from "../assets/img/canvasImage2.jpg";
 import Gallery from "../components/gallery";
-import image1 from "./../assets/img/pro-1.jpg";
-import image2 from "./../assets/img/pro-2.jpg";
-import image3 from "./../assets/img/pro-3.jpg";
-import image4 from "./../assets/img/pro-4.jpg";
 import Slider from "../components/imageSlider";
 import ShapeCarousel from "../components/shapeCarousel";
+import axios from "axios";
 
 const Home = () => {
-  const items = [
-    {
-      id: 1,
-      imageUrl: image1,
-      text: "DRESSES",
-      path: "/dresses",
-    },
-    {
-      id: 2,
-      imageUrl: image2,
-      text: "SHORTS",
-      path: "/shorts",
-    },
-    {
-      id: 3,
-      imageUrl: image3,
-      text: "TOPS",
-      path: "/tops",
-    },
-    {
-      id: 4,
-      imageUrl: image4,
-      text: "SETS",
-      path: "/sets",
-    },
-  ];
+  const [products, setProducts] = useState([]);
+  const [categories, setCategories] = useState([]);
+
+  const fetchProducts = async () => {
+    try {
+      const response = await axios.get("http://localhost:5000/api/products");
+      const activeProducts = response.data.filter(
+        (product) => product.Is_Active
+      );
+      setProducts(activeProducts);
+    } catch (error) {
+      console.error("Error fetching products:", error);
+    }
+  };
+
+  const fetchCategories = async () => {
+    try {
+      const response = await axios.get("http://localhost:5000/api/categories");
+      setCategories(response.data.slice(0, 4));
+    } catch (error) {
+      console.error("Error fetching categories:", error);
+    }
+  };
+
+  useEffect(() => {
+    fetchProducts();
+    fetchCategories();
+  }, []);
+
   return (
     <div>
       <Video />
-      <Slider title={"NEW IN"} />
+      <Slider title={"NEW IN"} productsData={products} />
       <CanvasImageWithText
         imageUrl={canvasImage1}
         path={"/mez"}
         title={"JANUARY 24 STOCKS"}
         description={"CULT FAVOURITES ARE BACK"}
       />
-      <Slider title={"MEZ"} />
+      <Slider title={"MEZ"} productsData={products} />
+
       <section className="my-5">
         <div className="bg-banner flex items-center justify-center h-[60px] tracking-wider font-bold">
           <p>INSPIRING CREATIVITY & CONFIDENCE THROUGH STYLE</p>
         </div>
       </section>
       <CanvasImage imageUrl={canvasImage2} path={"/shop"} />
-      <Gallery items={items} />
+      <Gallery items={categories} />
       <ShapeCarousel />
     </div>
   );
