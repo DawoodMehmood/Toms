f507f7cf69b4a26dcd5875e44aa7cb5bd32c1749
Muhammad Abdullah Khan Abbasi <rykabdullah7@gmail.com>
2024-03-12 14:01:28 +0500

    hover static navbar

--- frontend/src/components/navbar.jsx
+++ frontend/src/components/navbar.jsx
@@ -8,26 +8,67 @@ import Banner from "./announcementBanner";
 import HeartWithBadge from "./heartWithBadge";
 import CartWithBadge from "./cartWithBadge";
 import { Link } from "react-router-dom";
+import axios from "axios";
 
 const Navbar = () => {
   const [isMenuOpen, setIsMenuOpen] = useState(false);
+  const [isClothingDropdownOpen, setIsClothingDropdownOpen] = useState(false);
+  const [categories, setCategories] = useState([]);
+  const [subCategories, setSubCategories] = useState([]);
+
+  const fetchCategories = async () => {
+    try {
+      const response = await axios.get("http://localhost:5000/api/categories");
+      setCategories(response.data.slice(0, 4));
+    } catch (error) {
+      console.error("Error fetching categories:", error);
+    }
+  };
+
+  const fetchSubCategories = async () => {
+    try {
+      const response = await axios.get(
+        "http://localhost:5000/api/subcategories"
+      );
+      setCategories(response.data.slice(0, 4));
+    } catch (error) {
+      console.error("Error fetching categories:", error);
+    }
+  };
+
+  console.log(subCategories);
+  useEffect(() => {
+    fetchCategories();
+    fetchSubCategories();
+  }, []);
+
+  const toggleMenu = () => {
+    setIsMenuOpen(!isMenuOpen);
+  };
+
+  const toggleClothingDropdown = () => {
+    setIsClothingDropdownOpen(!isClothingDropdownOpen);
+  };
+
   return (
     <section>
       <Banner />
-      <nav>
-        <div className="bg-white shadow-md">
-          <div className="grid grid-cols-3 p-7">
-            <div
-              className=" cursor-pointer lg:opacity-0 lg:pointer-events-none"
-              onClick={() => setIsMenuOpen(!isMenuOpen)}
-            >
-              {isMenuOpen ? (
-                <RxCross1 size={25} />
-              ) : (
-                <RxHamburgerMenu size={25} />
-              )}
+      <nav className="bg-white shadow-md ">
+        <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
+          <div className="grid grid-cols-3 py-4">
+            <div className="flex items-center gap-4 cursor-pointer ">
+              <div className="lg:hidden" onClick={toggleMenu}>
+                {isMenuOpen ? (
+                  <RxCross1 className="block h-6 w-6" />
+                ) : (
+                  <RxHamburgerMenu className="block h-6 w-6" />
+                )}
+              </div>
+              <div className="sm:hidden flex items-center gap-2">
+                <GoSearch size={20} />
+              </div>
             </div>
-            <div className="text-center">
+            <div className=" flex justify-center items-center">
               <Link to={"/"}>
                 <img
                   src={logo}
@@ -36,17 +77,44 @@ const Navbar = () => {
                 />
               </Link>
             </div>
-            <div className="text-gray-300 flex items-center justify-end gap-2">
-              <GoSearch size={20} />
-              <span className="text-sm">SEARCH</span>
+            <div className="flex items-center justify-end gap-2">
+              <div className="hidden sm:flex items-center gap-2">
+                <GoSearch size={20} />
+                <span className="text-sm small-size">SEARCH</span>
+              </div>
               <HeartWithBadge count={15} />
               <CiUser size={26} />
               <CartWithBadge count={1} />
             </div>
           </div>
-          <div className="py-5 px-7 flex items-end justify-between text-sm text-gray-500">
+
+          <div className="hidden lg:flex py-5 px-7 items-end small-size justify-between text-sm text-gray-500">
             <div>NEW</div>
-            <div>CLOTHING</div>
+            <div
+              onMouseEnter={toggleClothingDropdown}
+              onMouseLeave={toggleClothingDropdown}
+            >
+              <span>CLOTHING</span>
+              {isClothingDropdownOpen && (
+                <div className="flex flex-row gap-12 justify-start absolute left-0 right-0 bg-white shadow-lg mt-2 p-4 z-10">
+                  <div className="flex flex-col gap-1 px-4 py-2">
+                    <a className="font-semibold my-2">Dresses</a>
+                    <a>Mini Dresses</a>
+                    <a>Jumbo Dresses</a>
+                  </div>
+                  <div className="flex flex-col gap-1 px-4 py-2">
+                    <a className="font-semibold my-2">Dresses</a>
+                    <a>Mini Dresses</a>
+                    <a>Jumbo Dresses</a>
+                  </div>
+                  <div className="flex gap-1 flex-col px-4 py-2">
+                    <a className="font-semibold my-2">Dresses</a>
+                    <a>Mini Dresses</a>
+                    <a>Jumbo Dresses</a>
+                  </div>
+                </div>
+              )}
+            </div>
             <div>MOST LOVED</div>
             <div>CAMPAIGNS</div>
             <div>BACK IN STORE</div>
@@ -54,6 +122,31 @@ const Navbar = () => {
             <div>ACCESSORIES</div>
             <div>SALE</div>
           </div>
+
+          {isMenuOpen && (
+            <div className="lg:hidden py-5 px-7 flex flex-col items-start text-sm text-gray-500">
+              <div>NEW</div>
+              <div
+                onMouseEnter={toggleClothingDropdown}
+                onMouseLeave={toggleClothingDropdown}
+              >
+                <span>CLOTHING</span>
+                {isClothingDropdownOpen && (
+                  <div className="absolute bg-white shadow-lg mt-2 py-2 w-48 z-10">
+                    <div className="px-4 py-2">Category 1</div>
+                    <div className="px-4 py-2">Category 2</div>
+                    <div className="px-4 py-2">Category 3</div>
+                  </div>
+                )}
+              </div>
+              <div>MOST LOVED</div>
+              <div>CAMPAIGNS</div>
+              <div>BACK IN STORE</div>
+              <div>EDITS</div>
+              <div>ACCESSORIES</div>
+              <div>SALE</div>
+            </div>
+          )}
         </div>
       </nav>
     </section>
