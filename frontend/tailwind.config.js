25b589a97d86831db60325a9e7cbd0e612869073
Muhammad Abdullah Khan Abbasi <rykabdullah7@gmail.com>
2024-03-13 15:20:24 +0500

    added some colors

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -6,6 +6,9 @@ module.exports = {
       colors: {
         banner: "#faf5ef",
         orangeColor: "#f69420",
+        darkGrey: "#555555",
+        pantone: "#9EC1CC",
+        darkPantone: "#4F8596",
       },
       fontSize: {
         xxs: ".625rem",
