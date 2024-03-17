79d057a366e82af0f444bc8576b12699019f0c70
Dawood Mehmood <dawoodmehmood52222@gmail.com>
2024-03-18 00:27:49 +0500

    introduced new associations and cascading on delete

--- backend/src/models/association‏Model.js
+++ backend/src/models/association‏Model.js
@@ -1,21 +1,37 @@
-const Product = require("./productModel");
-const Category = require("./categoryModel");
-const Subcategory = require("./subcategoryModel");
-const Color = require("./colorModel");
+import Product from "./productModel.js";
+import Category from "./categoryModel.js";
+import Subcategory from "./subcategoryModel.js";
+import Color from "./colorModel.js";
+import Measurement from "./measurementModel.js";
 
-exports.setupAssociations = () => {
-  Category.hasMany(Subcategory, { foreignKey: "category_id" });
-  Subcategory.belongsTo(Category, { foreignKey: "category_id" });
+export const setupAssociations = () => {
+  Category.hasMany(Subcategory, {
+    foreignKey: "category_id",
+    onDelete: "CASCADE",
+  });
+  Subcategory.belongsTo(Category, {
+    foreignKey: "category_id",
+  });
 
-  Subcategory.hasMany(Product, { foreignKey: "subcategory_id" });
-  Product.belongsTo(Subcategory, { foreignKey: "subcategory_id" });
+  Subcategory.hasMany(Product, {
+    foreignKey: "subcategory_id",
+    onDelete: "CASCADE",
+  });
+  Product.belongsTo(Subcategory, {
+    foreignKey: "subcategory_id",
+  });
 
   Color.belongsToMany(Product, {
     through: "product_colors",
     foreignKey: "color_id",
+    onDelete: "CASCADE",
   });
   Product.belongsToMany(Color, {
     through: "product_colors",
     foreignKey: "product_id",
+    onDelete: "CASCADE",
   });
+
+  Product.belongsToMany(Measurement, { through: "ProductMeasurements" });
+  Measurement.belongsToMany(Product, { through: "ProductMeasurements" });
 };
