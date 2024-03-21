3f7011a6cf4c0d30548b723af6d00d276fb0db45
Dawood Mehmood <dawoodmehmood52222@gmail.com>
2024-03-21 05:43:45 +0500

    axios api calling utility

---
+++ frontend/src/utils/apiCalling.js
@@ -0,0 +1,34 @@
+// src/utils/api.js
+import axios from "axios";
+import useAuthStore from "../store/cartStore";
+
+const baseURL = process.env.REACT_APP_API_URL;
+console.log(`Base URL: ${baseURL}`);
+
+// Instance for public requests
+export const publicAPI = axios.create({
+  baseURL: baseURL,
+  headers: {
+    "Content-Type": "application/json",
+  },
+});
+
+// Instance for authenticated requests
+export const authAPI = axios.create({
+  baseURL: baseURL,
+  headers: {
+    "Content-Type": "application/json",
+  },
+});
+
+// Use Zustand store to manage JWT token in Axios interceptor
+authAPI.interceptors.request.use(
+  (config) => {
+    const token = useAuthStore.getState().token; // Get token from Zustand store
+    if (token) {
+      config.headers.Authorization = `Bearer ${token}`;
+    }
+    return config;
+  },
+  (error) => Promise.reject(error)
+);
