6236caddbeb541ae158adf447f103c1ff984ebd3
Dawood Mehmood <dawoodmehmood52222@gmail.com>
2024-03-21 05:42:35 +0500

    added auth initializer in index

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -4,13 +4,17 @@ import "./index.css";
 import App from "./App";
 import { IconContext } from "react-icons";
 import { BrowserRouter } from "react-router-dom";
+import AuthInitializer from "./components/authInitializer";
+
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
     <BrowserRouter>
-      <IconContext.Provider value={{ className: "small-size" }}>
+      <AuthInitializer>
+        {/* <IconContext.Provider value={{ className: "small-size" }}> */}
         <App />
-      </IconContext.Provider>
+        {/* </IconContext.Provider> */}
+      </AuthInitializer>
     </BrowserRouter>
   </React.StrictMode>
 );
