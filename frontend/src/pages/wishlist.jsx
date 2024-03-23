10ef14d4bbf6c964832be7bed1caf3281a1f9506
Dawood Mehmood <dawoodmehmood52222@gmail.com>
2024-03-24 00:26:33 +0500

    made wishlist page dynamic

--- frontend/src/pages/wishlist.jsx
+++ frontend/src/pages/wishlist.jsx
@@ -1,54 +1,68 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import WishlistCard from "./../components/wishlistCard"; // Assuming WishlistCard.js is in the same directory
+import SliderCard from "../components/sliderCard";
+import { publicAPI } from "../utils/apiCalling";
+import useFavoritesStore from "../store/favouritesStore";
 
 const Wishlist = () => {
-  // Hardcoded products for demonstration
-  const wishlist = [
-    {
-      id: 1,
-      name: "Product 1",
-      price: "$19.99",
-      image:
-        "https://cdn.shopify.com/s/files/1/2682/6366/files/2024.01.19_VRGGRL_395_1024x1024.jpg?v=1709097269",
-    },
-    {
-      id: 2,
-      name: "Product 2",
-      price: "$29.99",
-      image:
-        "https://cdn.shopify.com/s/files/1/2682/6366/files/POS_8485_1024x1024.jpg?v=1708404297",
-    },
-    {
-      id: 3,
-      name: "Product 3",
-      price: "$39.99",
-      image:
-        "https://cdn.shopify.com/s/files/1/2682/6366/files/2023.09.29_VRGGRL_PINK_CREAM_BROWN_CARDIGAN_101_1024x1024.jpg?v=1697857466",
-    },
-  ];
+  const [products, setProducts] = useState([]);
+  const favorites = useFavoritesStore((state) => state.favorites);
+  const removeAllFavorites = useFavoritesStore(
+    (state) => state.removeAllFavorites
+  );
+  const isDisabled = favorites.length === 0;
+
+  useEffect(() => {
+    if (favorites.length > 0) {
+      fetchFavoriteProducts();
+    }
+  }, [favorites]);
+
+  const fetchFavoriteProducts = async () => {
+    if (favorites.length === 0) {
+      setProducts([]);
+      return;
+    }
+    try {
+      const response = await publicAPI.post("/products/byIds", {
+        ids: favorites,
+      });
+      setProducts(response.data);
+    } catch (error) {
+      console.error("Failed to fetch favorite products", error);
+    }
+  };
 
   return (
-    <div className="mx-5 md:mx-24  my-12">
-      <h1 className="text-3xl font-bold">My Wishlist</h1>
-      <div className="flex justify-center my-8 gap-5">
-        <button className="border-gray-600 text-gray-600 border font-medium py-2 px-4 ">
+    <div className="mx-5 md:mx-24 my-12">
+      <div className="flex justify-between my-8 gap-5">
+        <h1 className="text-3xl font-bold">My Wishlist</h1>
+        {/* <button className="border-gray-600 text-gray-600 border font-medium py-2 px-4 ">
           SHARE WISHLIST
-        </button>
-        <button className="border-black border font-medium py-2 px-4">
+        </button> */}
+        <button
+          className={`border font-medium py-2 px-4 ${
+            isDisabled ? "disableThing" : "text-black border-black"
+          }`}
+          onClick={removeAllFavorites}
+          disabled={isDisabled}
+        >
           CLEAR ALL
         </button>
       </div>
       <div className="hidden md:grid grid-cols-1 md:grid-cols-3 gap-12 px-5">
-        {wishlist.map((product) => (
-          <WishlistCard key={product.id} product={product} />
-        ))}
+        {favorites.length > 0 &&
+          products.map((product) => (
+            <SliderCard key={product.id} product={product} />
+          ))}
       </div>
       <div className="md:hidden mt-4">
         {/* Mobile view: Two columns of cards */}
         <div className="grid grid-cols-2 gap-4">
-          {wishlist.map((product) => (
-            <WishlistCard key={product.id} product={product} />
-          ))}
+          {favorites.length > 0 &&
+            products.map((product) => (
+              <SliderCard key={product.id} product={product} />
+            ))}
         </div>
       </div>
     </div>
