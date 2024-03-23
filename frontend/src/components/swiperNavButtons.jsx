7acaefdf5530fc12015c57f79a05ea1f0a1dcb22
Dawood Mehmood <dawoodmehmood52222@gmail.com>
2024-03-24 00:22:59 +0500

    applied condition based disabled class to swiper buttons

--- frontend/src/components/swiperNavButtons.jsx
+++ frontend/src/components/swiperNavButtons.jsx
@@ -2,19 +2,25 @@ import React from "react";
 import { useSwiper } from "swiper/react";
 import { RxCaretLeft, RxCaretRight } from "react-icons/rx";
 
-export const SwiperNavButtons = ({ prevRef, nextRef }) => {
+export const SwiperNavButtons = ({ isDisabled }) => {
   const swiper = useSwiper();
 
   return (
     <div className="flex justify-end mb-4">
       <button
-        className={`rounded-full bg-banner flex items-center justify-center mx-4`}
+        className={`rounded-full bg-banner flex items-center justify-center mx-4 ${
+          isDisabled ? "disableThing" : ""
+        }`}
+        disabled={isDisabled}
         onClick={() => swiper.slidePrev()}
       >
         <RxCaretLeft size={32} />
       </button>
       <button
-        className={`rounded-full bg-banner flex items-center justify-center`}
+        className={`rounded-full bg-banner flex items-center justify-center ${
+          isDisabled ? "disableThing" : ""
+        }`}
+        disabled={isDisabled}
         onClick={() => swiper.slideNext()}
       >
         <RxCaretRight size={32} />
