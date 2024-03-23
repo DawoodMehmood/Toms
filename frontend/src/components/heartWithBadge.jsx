7f292d62aa8e7173f37f1cad24c81e5db5ffbf5d
Dawood Mehmood <dawoodmehmood52222@gmail.com>
2024-03-24 00:23:21 +0500

    made heart with badge dynamic

--- frontend/src/components/heartWithBadge.jsx
+++ frontend/src/components/heartWithBadge.jsx
@@ -1,13 +1,17 @@
 import React from "react";
 import { IoMdHeartEmpty } from "react-icons/io";
+import useFavoritesStore from "../store/favouritesStore";
 
-const HeartWithBadge = ({ count }) => {
+const HeartWithBadge = () => {
+  const favorites = useFavoritesStore((state) => state.favorites);
   return (
     <div className="relative">
       <IoMdHeartEmpty size={25} />
-      <span className="absolute bottom-0 left-4 h-4 w-4 text-xxs rounded-full bg-orangeColor text-white flex items-center justify-center">
-        {count}
-      </span>
+      {favorites.length > 0 && (
+        <span className="absolute bottom-0 left-4 h-4 w-4 text-xxs rounded-full bg-orangeColor text-white flex items-center justify-center">
+          {favorites.length}
+        </span>
+      )}
     </div>
   );
 };
