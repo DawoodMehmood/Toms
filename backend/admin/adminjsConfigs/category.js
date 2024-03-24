d38c68371d6dcd22428e061fefba57f672506312
Dawood Mehmood <dawoodmehmood52222@gmail.com>
2024-03-24 10:47:20 +0500

    modified category options for adminjs display

--- backend/admin/adminjsConfigs/category.js
+++ backend/admin/adminjsConfigs/category.js
@@ -1,49 +1,38 @@
-import Category from "../../src/models/categoryModel.js";
-
-const CategoryAdminConfig = {
-  resource: Category,
-  options: {
-    actions: {
-      show: {
-        showInDrawer: true,
-      },
+const CategoryOptions = {
+  actions: {
+    show: {
+      showInDrawer: true,
     },
-    sort: {
-      sortBy: "category_id",
-      direction: "desc",
+  },
+  sort: {
+    sortBy: "category_id",
+    direction: "desc",
+  },
+  listProperties: ["category_id", "name", "is_active", "parent_id", "sorting"],
+  // editProperties: [
+  //   "product_name",
+  // ],
+  filterProperties: [
+    "category_id",
+    "name",
+    "is_active",
+    "parent_id",
+    "sorting",
+  ],
+  properties: {
+    category_id: {
+      isVisible: { list: true, filter: true, show: true, edit: false },
+      position: 100,
     },
-    listProperties: [
-      "category_id",
-      "name",
-      "is_active",
-      "parent_id",
-      "Sorting",
-    ],
-    // editProperties: [
-    //   "product_name",
-    // ],
-    filterProperties: [
-      "category_id",
-      "name",
-      "is_active",
-      "parent_id",
-      "Sorting",
-    ],
-    properties: {
-      category_id: {
-        isVisible: { list: true, filter: true, show: true, edit: false },
-        position: 100,
-      },
-      name: {
-        isTitle: true,
-        position: 200,
-      },
-      is_active: {
-        // isVisible: { list: true, filter: true, show: true, edit: false },
-        position: 50,
-      },
+    name: {
+      isTitle: true,
+      position: 200,
+    },
+    is_active: {
+      // isVisible: { list: true, filter: true, show: true, edit: false },
+      position: 50,
     },
   },
 };
 
-export default CategoryAdminConfig;
+export default CategoryOptions;
