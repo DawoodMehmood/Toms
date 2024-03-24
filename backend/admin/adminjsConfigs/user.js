f92e8b130aecfb8bd0f073a6227aa334b29c3b35
Dawood Mehmood <dawoodmehmood52222@gmail.com>
2024-03-24 10:46:15 +0500

    added user model

---
+++ backend/admin/adminjsConfigs/user.js
@@ -0,0 +1,52 @@
+import User from "../../src/models/userModel.js";
+
+const CategoryAdminConfig = {
+  resource: User,
+  options: {
+    navigation: {
+      icon: "Tag",
+    },
+    actions: {
+      show: {
+        showInDrawer: true,
+      },
+    },
+    sort: {
+      sortBy: "category_id",
+      direction: "desc",
+    },
+    listProperties: [
+      "category_id",
+      "name",
+      "is_active",
+      "parent_id",
+      "sorting",
+    ],
+    // editProperties: [
+    //   "product_name",
+    // ],
+    filterProperties: [
+      "category_id",
+      "name",
+      "is_active",
+      "parent_id",
+      "sorting",
+    ],
+    properties: {
+      category_id: {
+        isVisible: { list: true, filter: true, show: true, edit: false },
+        position: 100,
+      },
+      name: {
+        isTitle: true,
+        position: 200,
+      },
+      is_active: {
+        // isVisible: { list: true, filter: true, show: true, edit: false },
+        position: 50,
+      },
+    },
+  },
+};
+
+export default CategoryAdminConfig;
